9314a2915283a1d9e04e90297be17f9c4037b524
mitchamador <vicokenator@gmail.com>
2020-01-09 10:08:12 +0300

    Wive-ng-mt for NEWIFI-D2 router

---
+++ etc/scripts/services_restart.sh
@@ -0,0 +1,235 @@
+#!/bin/sh
+
+##########################################################
+# This scipt restart needed services                     #
+#							 #
+# MODES:						 #
+# all -  call from internel.sh - full reconfigure	 #
+# dhcp - call from dhcp script after full renew		 #
+# pppd - call from ip-up/down scripts			 #
+# misc - call from webui by apply change in misc page	 #
+##########################################################
+
+
+# prevent double start configure in one time
+while [ -e /tmp/servicerestart_runing ]; do
+    # Sleep until file does exists/is created
+    usleep 500000
+done
+echo $$ > /tmp/servicerestart_runing
+
+# include global config
+. /etc/scripts/global.sh
+
+LOG="logger -t services"
+MODE="$1"
+
+$LOG "Restart needed services and scripts. Mode $MODE"
+
+##########################################################
+# Always reload shaper and netfilter rules		 #
+##########################################################
+    service shaper restart
+    service iptables restart
+
+##########################################################
+# full reconfigure ipv6 if settings apply or first start
+# reconfigure ipv6 tunnels after wan/vpn adress renew
+# and reconfigure and start radvd/dhcp6s at apply in misc
+##########################################################
+if [ "$MODE" = "all" ]; then
+    service six restart
+elif [ "$IPv6OpMode" = "2" -o "$IPv6OpMode" = "3" ]  && [ "$MODE" = "dhcp" -o "$MODE" = "pppd" ]; then
+    service six restart
+elif [ "$MODE" = "misc" ]; then
+    service six dhcpradvdreconf
+    service watchdog restart
+fi
+
+##########################################################
+# Regenerate resolv for all times
+##########################################################
+service resolv restart
+
+##########################################################
+# Always reload some services				 #
+##########################################################
+if [ -e /etc/init.d/parprouted ]; then
+    service parprouted restart
+fi
+if [ -e /etc/init.d/dynroute ]; then
+    service dynroute restart
+fi
+service dnsserver reload
+service ddns restart
+service ntp restart
+service miniupnpd restart
+
+##########################################################
+# restart nodogsplash/chillispot hotspot daemon		 #
+# need always restart - netfilter reconfigure by daemon  #
+##########################################################
+if [ -e /bin/nodogsplash ]; then
+    service nodogsplash restart
+fi
+if [ -e /bin/chilli ]; then
+    service chillispot restart
+fi
+
+##########################################################
+# Need restart this:					 #
+# 1) if physycal wan ip adress change			 #
+# 2) if apply changes from web				 #
+# 3) if full reconfigure				 #
+##########################################################
+if [ "$MODE" = "dhcp" ] || [ "$MODE" = "misc" ] || [ "$MODE" = "all" ]; then
+    if [ -e /etc/init.d/igmp_proxy ]; then
+        service igmp_proxy restart
+    fi
+    if [ -e /etc/init.d/xupnpd ]; then
+	service xupnpd restart
+    fi
+    if [ -e /etc/init.d/udpxy ]; then
+	service udpxy restart
+    fi
+fi
+
+##########################################################
+# Need restart this:					 #
+# 1) if misc chane apply				 #
+# 2) full reconfigure					 #
+##########################################################
+if [ "$MODE" = "misc" ] || [ "$MODE" = "all" ]; then
+    service kext restart
+    if [ -e /bin/ralinkiappd ]; then
+	service iappd restart
+    fi
+    if [ -e /bin/bndstrg ]; then
+	service bndstr restart
+    fi
+    if [ -e /etc/init.d/arpwatch ]; then
+	service arpwatch restart
+    fi
+    if [ -e /etc/init.d/lld2d ]; then
+	service lld2d restart
+    fi
+    if [ -e /etc/init.d/snmpd ]; then
+        service snmpd restart
+    fi
+    if [ -e /etc/init.d/crontab ]; then
+        service crontab restart
+    fi
+    if [ -e /etc/init.d/inetd ]; then
+        service inetd restart
+    fi
+    if [ -e /etc/init.d/transmission ]; then
+        service transmission restart
+    fi
+    if [ -e /etc/init.d/iperf ]; then
+	service iperf restart
+    fi
+fi
+
+##########################################################
+# Reconfigure and restart samba:			 #
+# 1) if recive wins server adress from dhcp		 #
+# 2) if full reconfigure
+##########################################################
+if [ "$MODE" = "all" ] || [ "$MODE" = "dhcp" ] && [ -e /tmp/wins.dhcp ]; then
+    service samba restart
+fi
+
+# renew /etc/udhcpd.conf and restart dhcp server
+if [ "$MODE" = "misc" -o "$MODE" = "all" ]; then
+    service dhcpd restart
+elif [ "$dnsPEnabled" != "1" -a "$wan_static_dns" != "on" ] && [ "$MODE" = "pppd" -o "$MODE" = "dhcp" ]; then
+    # if dnsmasq or static dns disabled and mode=pppd/dhcp (renew/reconnect ISP)
+    sleep 3
+    service dhcpd restart
+fi
+
+##########################################################
+# Always send CDP/SONMP/FDP/EDP to ISP			 #
+##########################################################
+if [ -e /bin/cdp-send ]; then
+	service cdp restart
+fi
+if [ -e /bin/lldpd ]; then
+	service lldpd restart
+fi
+
+##########################################################
+# After physical reinit 				 #
+##########################################################
+if [ "$MODE" = "all" ]; then
+    ##########################################################
+    # This is hook for exec user script after connection     #
+    # configured. May be used for add scripts needed by some #
+    # provides.                                              #
+    # Example: http://wive-ng.sf.net/downloads/wan_up.sh     #
+    # This script load external routes for www.kvidex.ru ISP #
+    ##########################################################
+    if [ -e /etc/scripts/wan_up.sh ]; then
+	$LOG "Call user /etc/scripts/wan_up.sh script."
+	sh /etc/scripts/wan_up.sh
+    fi
+    ##########################################################
+    # in dhcp client mode restart from dhcp script           #
+    # in static/zeroconf/pure pppoe mode need restart anyway #
+    ##########################################################
+    if [ "$wanConnectionMode" != "DHCP" ] || [ "$purepppoemode" = "1" ]; then
+	service vpnhelper restart
+    fi
+fi
+
+###########################################################
+# need restart L2TP server every netfilter rules replaced
+# or ppp session to uplink restarted or new adress recieved
+###########################################################
+service vpnserver restart
+
+
+##########################################################
+# reload radius auth daemon				 #
+##########################################################
+if [ -e /bin/radiusd ]; then
+    service radius reload
+fi
+
+##########################################################
+# restart 802.1x wireless daemon			 #
+##########################################################
+if [ -e /bin/rt2860apd ]; then
+    service apd restart
+fi
+
+###########################################################
+# last stage after apply all changes restart cwmp client
+###########################################################
+if [ -e /bin/cwmpd ]; then
+    service cwmpd restart
+fi
+
+###########################################################
+# always rebalance irq by cpus				  #
+###########################################################
+if [ -e /bin/irqbalance ]; then
+    service irqbalance restart
+fi
+
+###########################################################
+# force check updates					  #
+###########################################################
+if [ -e /bin/fwupdate ]; then
+    fwupdate -c check  > /dev/null 2>&1 &
+fi
+
+###########################################################
+# start adblock service script				  #
+###########################################################
+if [ -e /etc/scripts/config-adblock.sh ]; then
+    service dnsserver adrestart
+fi
+
+# remove running flag
+rm -f /tmp/servicerestart_runing
---
+++ etc/scripts/unload_all.sh
@@ -0,0 +1,100 @@
+#!/bin/sh
+
+# Description: Wive-NG Megakill script..
+
+#############################
+# do not stop syslog/klog !
+#############################
+
+stop_serv="hotplug bndstr radius chillispot transmission samba xupnpd dynroute crontab ddns udpxy miniupnpd \
+	    igmp_proxy ntp snmpd parprouted irqbalance lld2d lldpd cdp iappd dnsserver inetd iperf"
+
+# kill dhcp servers directly to avoid unneded deconfig and flush conntrack
+kill_apps="udhcpd dhcp6s radvd chilli_stat haveged butcheck"
+
+term_sessions() {
+    eval `nvram_buf_get 2860 RemoteManagement`
+
+    if [ "$1" = "REBOOT" ] || [ "$RemoteManagement" != "2" ]; then
+	# VPN force disconnect from server
+	killall -q -SIGHUP pppd
+	# wait send disconnetc
+	usleep 500000
+	# terminate pppd daemon
+	while killall -q pppd; do
+	    usleep 200000
+	    killall -q -SIGKILL pppd
+	done
+	# terminate xl2tp daemon
+	while killall -q xl2tpd; do
+	    usleep 200000
+	    killall -q -SIGKILL xl2tpd
+	done
+	# send release to DHCP server
+	killall -q -SIGUSR2 udhcpc
+	# kill dhcp6c
+	killall -q -SIGKILL dhcp6c
+    fi
+}
+
+unload_apps() {
+    echo "Stop services."
+    # first stop adfilter (more and more ram usage)
+    service dnsserver adstop
+    for serv in $stop_serv
+    do
+	service $serv stop
+    done
+
+    # free caches
+    sync
+
+    echo "Kill aplications." # second step terminate and kill application if not correct stopped
+    for app in $kill_apps
+    do
+	pid=`pidof $app`
+	if [ "$pid" != "" ]; then
+    	    while killall -q $app; do
+        	usleep 10000
+        	killall -q -SIGKILL $app
+    	    done
+	fi
+    done
+}
+
+remove_web() {
+    if [ -d /tmp/web ]; then
+	echo "Remove web pages from tmpfs."
+	rm -rf /tmp/web
+	ln -sf /web /tmp/web
+    fi
+}
+
+disable_hotplug() {
+    if [ -e /proc/sys/kernel/hotplug ]; then
+	echo "" > /proc/sys/kernel/hotplug
+    fi
+}
+
+umount_all() {
+    if [ -e /etc/scripts/umount_all.sh ]; then
+	/etc/scripts/umount_all.sh
+    fi
+}
+
+sysctl -w vm.dirty_expire_centisecs=10
+
+# terminate pppoe and
+term_sessions
+
+# unload all applications
+unload_apps
+
+# disable hotplug helper
+disable_hotplug
+
+# umount all particions and disable swap
+umount_all
+
+# remove web pages from tmpfs and link to rootfs
+remove_web
